e5842b4547e2cfd296be4606192712582efc5db4
David/Adam Dai <wdai1@paypalcorp.com>
2018-08-30 11:43:24 +0800

    make a hash script to do SHA256

--- fileSystem/hash.py
+++ fileSystem/hash.py
@@ -11,23 +11,36 @@ def stronghash(text):
     Computes the MD5 hash of a text string
     """
     # print(hashlib.algorithms_guaranteed)
-    h = hashlib.sha3_256()
+    h = hashlib.sha256()
     h.update(text.encode('utf-8'))
     return h.hexdigest()
 
 
 if __name__ == '__main__':
-    import pathlib
+    '''import pathlib
+    
     source_file = (pathlib.Path(__file__) / __file__).resolve()
     with source_file.open('r') as handle:
-        content = handle.read()
+       content = handle.read()
 
     # Compute MD5 hash of some file content
     original_md5 = stronghash(content)
     print('Original content hash: {}'.format(original_md5))
 
     # Now let's change the contents and recompute the MD5
-    content += 'yohoo'
+    content += 'yohoo'  
     modified_hash = stronghash(content)
-    print('Modified content hash: {}'.format(modified_hash))
+    '''
+    import argparse
+
+    arg_parser = argparse.ArgumentParser()
+
+    arg_parser.add_argument("api_user", type=str, nargs='?', default="check_string_for_empty")
+    args = arg_parser.parse_args()
+
+    if args.api_user == 'check_string_for_empty':
+        print('please provide an api user name. e.g.: davidgadam_api1.gmail.com')
+    else:
+        result_hash=stronghash(args.api_user)
+        print('The CALLER_ACCT_IDENTIFIER is: {}'.format(result_hash))
 
